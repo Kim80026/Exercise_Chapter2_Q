203161aa7eb6aaaaae6c7021353f5bfa456bc64c
k80026 <k80026@gmail.com>
2023-02-03 15:36:10 +0900

    v1.01

--- Q2.py
+++ Q2.py
@@ -7,6 +7,19 @@ def solution(numbers):
             answer += i
     return answer
 
+def solution2(numbers):
+    ans = 0
+    tmp = 0
+    for i in range(0, 10):
+        ans += i
+    
+    for j in numbers:
+        tmp += j
+    
+    return ans - tmp
+
 numbers = [1,2,3,4,6,7,8,0]
 answer = solution(numbers)
+answer2 = solution2(numbers)
 print(answer)
+print(answer2)
